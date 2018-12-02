02af3e808d1cf59df7a0c658fbdc7d57574bdc91
aidanak <19August1996>
2018-12-02 22:18:21 +0600

    user-agent rotation

--- amazon/amazon/settings.py
+++ amazon/amazon/settings.py
@@ -52,9 +52,9 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'amazon.middlewares.AmazonDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+    'amazon.middlewares.RotateUserAgentMiddleware': 110,
+}
 
 # Enable or disable extensions
 # See https://doc.scrapy.org/en/latest/topics/extensions.html
