e441125ddf5092b7107be96274555a24413d383c
aidanak <19August1996>
2018-12-02 21:52:15 +0600

    first commit

---
+++ amazon/amazon/spiders/word_searcher.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from scrapy.exceptions import CloseSpider
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+import scrapy
+
+
+class WordSearcherSpider(CrawlSpider):
+    name = 'word_searcher'
+    allowed_domains = ['www.amazon.com']
+    start_urls = ['https://www.amazon.com/']
+    searched_words = ["Virtue", "signalling", "is", "society", "version", "of", "Proof", "Stake"]
+    found_words = []
+    rules = [
+        Rule(LinkExtractor(allow=()),
+             callback='detail', follow=True)
+    ]
+
+    def detail(self, response):
+        for word in self.searched_words:
+                if word.lower() not in self.found_words:
+                    if word.lower() in str(response.body):
+                        print("Found word {0} at url {1}".format(word.lower(), response.url))
+                        self.found_words.append(word.lower())
+
+        if len(self.found_words) == len(self.searched_words):
+            raise CloseSpider('Found all words')
+
+
